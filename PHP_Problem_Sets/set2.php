24add6b5a567792fa42e128b02e9b60f4f36ce7d
Onur Melikoğlu <60974044+OBSoft-Onur@users.noreply.github.com>
2021-02-11 17:56:13 +0300

    Update

--- PHP_Problem_Sets/set2.php
+++ PHP_Problem_Sets/set2.php
@@ -8,35 +8,38 @@
 <body>
 
 <form action="set2.php" method="post">
-    <label for="">Lütfen sayı Giriniz =</label>
-    <input type="input" name="click" type="number">
+    <label>Lütfen sayı Giriniz =</label>
+    <input type="input" name="input" type="number">
     <button>Giriş</button>
 </form>
     
 </body>
 </html>
 
-<?php
 
-if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['click']))
+<?php 
+
+session_start();
+
+if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['input']))
     {
-        func();
-    }
-    function func()
-    {
-        $num = 6;
-        $result = 0;
-        for ($i = 1; $i < $num ; $i++) {
-            if($num%$i == 0){
-                $result += $i;
-                echo $i.'<br>';
-            }
-        }
-        if($result == $num){
-            echo $num.' is an excellent number!!!';
-        }else {
-            echo $num.' is not an excellent number!!!';
-}     
+        checkTheExcellentNumber($_POST['input']);
     }
 
-?>
+function checkTheExcellentNumber($num){
+		$result = 0;
+for ($i = 1; $i < $num ; $i++) {
+	if($num%$i == 0){
+		$result += $i;
+		echo "Divison of the number : ".$i.'<br>';
+	}
+}
+if($result == $num){
+	echo $num.' is an excellent number!!!';
+}else {
+	echo $num.' is not an excellent number!!!';
+}
+}
+
+
+ ?>
